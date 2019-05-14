c0ed03a0e4228db892e970773fa29080afb8528d
Samuel Schantz <Samuel Schantz@tbadigital.local>
2019-05-14 15:24:59 -0700

    restructuring how validation and buttons work

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -5,47 +5,40 @@ import shortid from 'shortid';
 class Home extends Component {
   state = {
     gameId: '',
-    user: ''
+    user: '',
+    joinGameInfo: false,
+    newGameInfo: true
   }
+  // When the user enters info both new game and join game are greyed out.
+  // When a name longer than 2 chars is entered new game becomes active
+  // When a code 10 chars long is entered for game id and there is a name longer than 2 chars that becomes active
+  // both become inactive if their standards are not met
 
-  inputHandler = () => {
-    const nameInput = document.getElementById('name-input');
-    const newGameButton = document.getElementById('new-game-button');
-    const joinGameButton = document.getElementById('join-game-button');
-        
-    let name = nameInput.value;
-      
+  nameInputHandler = (e) => {
+    let name = e.target.value;
+    let buttons = Array.from(document.getElementsByTagName('button'));
     if(name.length > 2){
-
+      buttons.forEach(function(button){ 
+        button.classList.remove("noClick");
+      })
     } else {
-
+      buttons.forEach(function(button){ 
+        button.classList.add("noClick");
+      })
     }
   }
 
-  newGame = () => {
-    const gameId    = shortid.generate();
-    const gameOwner = this.state.user;
-    this.props.firebase.database().ref('games/' + gameId).set({
-        gameOwner : gameOwner,
-        gameState : { started: false }
-    });
-  }
-
-  //Checks whether this name already exists for the given game
-  checkName = () => {
-  }
-
-  joinGame = () => {
-    this.checkName();
-  }
 
   render(){
     return(
       <div className="start-screen">
-        <input id="name-input" placeholder="Name required to start or join game" onInput={this.inputHandler} />
-        <input id="game-id-input" placeholder="Game ID required to join game" onInput={this.inputHandler} />
-        <button id="new-game-button" onClick={this.newGame} >New Game</button>
-        <button id="join-game-button" onClick={this.joinGame} >Join Game</button>
+        <p>Enter a name to get started</p>
+        <p>Name & Game ID required to join existing game.</p>
+        <input id="name-input" placeholder="Name required to start or join game" onInput={this.nameInputHandler} />
+        <button className="noClick" id="new-game-button" onClick={this.newGame} >New Game</button>
+        <button className="noClick" id="join-game-button" onClick={this.joinGame} >Join Game</button>
+        <input id="game-id-input" placeholder="Game ID required to join game" />
+        <p id="error-block"></p>
       </div>
     )
   }
