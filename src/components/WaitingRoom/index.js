34c60e8d575f00960c3bb363aa2733f1400c7fc4
Samuel Schantz <Samuel Schantz@tbadigital.local>
2019-05-17 11:53:55 -0700

    Took all functions out of useEffect since they do not need to run on render

--- src/components/WaitingRoom/index.js
+++ src/components/WaitingRoom/index.js
@@ -7,41 +7,46 @@ function WaitingRoom(props) {
     const [startGame, setStartGame] = useState(false);
     const [players, setData]        = useState([props.user]);
     const [usersObj, setUsers]      = useState({});
+    let [, updateState] = React.useState();
 
     const user        = props.user;
     const gameId      = props.gameId;
     const dbReference = props.firebase.database();
     const usersRef    = dbReference.ref('games/' + gameId + '/players');
     const gameRef     = dbReference.ref('games/' + gameId + '/gameState');
+
     let users         = {};
     // console.log(props.firebase.auth().currentUser);
 
     useEffect(() => {
-        usersRef.once('value')
-            .then((snapshot) => {
-              users = snapshot.val();
-              setUsers(users);
-            })
-            .then(() => {
-              let players = Object.keys(users);
-              setData(players);
-            })
     }, [])
+    //FIRST and second FIRST is the game creator and one to press start game
+    //When both are in waiting room both should have usersObj in state
+
+    //OBVIOUSLY react re-renders when setting state in useEffect()
 
     const handlePacket = (snapshot) => {
+        let newPlayer       = snapshot.val().playerName;
         let newPlayerData   = snapshot.val();
-        let newPlayer       = newPlayerData.playerName;
         if(!players.includes(newPlayer)){
             let newPlayers      = [...players, newPlayer];
-            setData(newPlayers); 
-            console.log('do we ever actually get here?') 
-            let newPlayersData  = {...usersObj, [newPlayer]: newPlayerData}
-            console.log(newPlayersData);
-            setUsers(newPlayersData);       
+            setData(newPlayers);
         }
     }
 
     usersRef.on('child_added', handlePacket);
+    
+    // console.log(usersObj);
+    // console.log(Object.keys(usersObj).length);
+    // if(Object.keys(usersObj).length > 1){
+    //     console.log('this should run')
+    //     usersRef.once('value')
+    //         .then((snapshot) => {
+    //           users = snapshot.val();
+    //           setUsers(users);
+    //           console.log(users);
+    //         })
+    // }
 
     const startGameHandler = () => {
         console.log('inside startGameHandler');
