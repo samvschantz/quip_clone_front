9e864da75cac7b239e78c8537d172f644f652a38
Samuel Schantz <Samuel Schantz@tbadigital.local>
2019-05-17 16:59:58 -0700

    Sorted out waiting room

--- src/components/StartGame/index.js
+++ src/components/StartGame/index.js
@@ -8,22 +8,32 @@ function StartGame(props) {
     const user        = props.user;
     const gameId      = props.gameId;
     const users       = props.users;
+    const playerNames = Object.keys(users);
+    const dbReference   = props.firebase.database();
+
 
     useEffect(() => {
       pointsDisplay();
     }, [])
 
+
+
     const pointsDisplay = () => {
       let rows = [];
-      console.log(users);
       for(let user in users){
         rows.push(user + ' has ' + users[user].points + ' points'); 
       }
       setPlayersDisplay(rows);
     }
 
-    const startGameHandler = () => {
+    function shuffle(array) {
+      for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+        [array[i], array[j]] = [array[j], array[i]]; // swap elements
+      }
     }
+
+    //build with no time limit initially
 
     return (
         <div>
@@ -31,6 +41,7 @@ function StartGame(props) {
           {playersPoints.map((player, index) => (
               <span key={index}>{player}</span>
           ))}
+          <p></p>
         </div>
     )
 }
