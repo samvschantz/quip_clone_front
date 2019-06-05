2001bcb6df1484ef7032b3f29c8cda96fef8d905
Samuel Schantz <Samuel Schantz@tbadigital.local>
2019-06-05 14:25:26 -0700

    fixed card counting issue during turns

--- src/components/Round/index.js
+++ src/components/Round/index.js
@@ -270,7 +270,7 @@ function Round(props) {
                  {
                     turn !== user ? <p> {turn} is judging this round!</p> : ''
                   }
-                  <p>cards have been played.</p>
+                  <p>{numPlayed} cards have been played.</p>
                   {!time ? ''
                   : <>
                       <div className="timer one">turns last 30 seconds</div>
